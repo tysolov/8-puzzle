e502fa553f2c2c592781bc74391eb71966868456
Tyson <tlove@ucr.edu>
2016-10-18 15:28:13 -0700

    working out data structure

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -1,8 +1,29 @@
 import a_star
 import re
 
-defaultPuzzle = [[1,2,3],[4,8,0],[7,6,5]]
+answerKey = {'1': (1, 1), '2': (1, 2), '3': (1, 3), '4': (2, 1), '5': (2, 2), '6': (2, 3), '7': (3, 1), '8': (3, 2), '0': (3, 3)}
+
+
 customPuzzle = []
+
+class node:
+    def __init__(self, row, col, num):
+        self.r = row
+        self.c = col
+        self.val = num
+
+    def __repr__(self):
+        return str(self.val)
+
+    def misplaced(self):
+        if
+        if answerKey[str(self.val)]==(self.r, self.c):
+            return 0
+        else: return 1
+
+    def manhattan(self):
+        goal = answerKey[str(self.val)]
+        return abs(self.r-goal[0]) + abs(self.c-goal[1])
 
 def doSearch(x):
     if x == 1: return a_star.ucs(customPuzzle)
@@ -16,13 +37,34 @@ def makePuzzle():
     print "    Enter your puzzle, use a zero to represent the blank"
     get = raw_input('    Enter the first row, use space or tabs between numbers ')
     temp = map(int, re.split(', | ', get))
-    customPuzzle.append(temp)
+    temp2 = []
+    i = 1
+    j = 1
+    for num in temp:
+            x = node(i, j, num)
+            temp2.append(x)
+            i += 1
+    i = 1
+    j = 2
+    customPuzzle.append(temp2)
     get = raw_input('    Enter the first row, use space or tabs between numbers ')
     temp = map(int, re.split(', | ', get))
-    customPuzzle.append(temp)
+    temp2 = []
+    for num in temp:
+            x = node(i, j, num)
+            temp2.append(x)
+            i += 1
+    i = 1
+    j = 3
+    customPuzzle.append(temp2)
     get = raw_input('    Enter the third row, use space or tabs between numbers ')
     temp = map(int, re.split(', | ', get))
-    customPuzzle.append(temp)
+    temp2 = []
+    for num in temp:
+            x = node(i, j, num)
+            temp2.append(x)
+            i += 1
+    customPuzzle.append(temp2)
     return customPuzzle
 
 def getPuzzle():
@@ -42,12 +84,15 @@ def getAlg():
     print "         3. A* with the Manhattan distance heuristic.\n"
     return doSearch(input('         '))
 
+defaultPuzzle = [[node(1,1,1),node(1,2,2),node(1, 3,3)],
+                 [node(2,1,4),node(2,2,8),node(2,3,0)],
+                 [node(3,1,7),node(3,2,6),node(3,3,5)]]
+
 def main():
     print "Welcome to Tyson Loveless' 8-puzzle solver."
     thePuzzle = getPuzzle()
     for row in thePuzzle:
         print row
     print getAlg()
-
 
 main()
