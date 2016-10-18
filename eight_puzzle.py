1ffcfaf8dec9af41fadca5db43b7aaa43fe01307
Tyson <tlove@ucr.edu>
2016-10-18 14:20:13 -0700

    UI updates

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -5,27 +5,35 @@ defaultPuzzle = [[1,2,3],[4,8,0],[7,6,5]]
 customPuzzle = []
 
 def doSearch(x):
-    return {
-        1 : a_star.ucs(customPuzzle),
-        2 : a_star.asmth(customPuzzle),
-        3 : a_star.asmdh(customPuzzle)
-    }.get(x, 'incorrect input')
-
-def getPuzzle(y):
-    if y == 1:
-        return defaultPuzzle
+    if x == 1: return a_star.ucs(customPuzzle)
+    elif x==2: return a_star.asmth(customPuzzle)
+    elif x==3: return a_star.asmdh(customPuzzle)
     else:
-        print "    Enter your puzzle, use a zero to represent the blank"
-        get = raw_input('    Enter the first row, use space or tabs between numbers ')
-        temp = map(int, re.split(', | ', get))
-        customPuzzle.append(temp)
-        get = raw_input('    Enter the first row, use space or tabs between numbers ')
-        temp = map(int, re.split(', | ', get))
-        customPuzzle.append(temp)
-        get = raw_input('    Enter the third row, use space or tabs between numbers ')
-        temp = map(int, re.split(', | ', get))
-        customPuzzle.append(temp)
-        return customPuzzle
+        print "incorrect input"
+        return getAlg()
+
+def makePuzzle():
+    print "    Enter your puzzle, use a zero to represent the blank"
+    get = raw_input('    Enter the first row, use space or tabs between numbers ')
+    temp = map(int, re.split(', | ', get))
+    customPuzzle.append(temp)
+    get = raw_input('    Enter the first row, use space or tabs between numbers ')
+    temp = map(int, re.split(', | ', get))
+    customPuzzle.append(temp)
+    get = raw_input('    Enter the third row, use space or tabs between numbers ')
+    temp = map(int, re.split(', | ', get))
+    customPuzzle.append(temp)
+    return customPuzzle
+
+def getPuzzle():
+    print "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle."
+    y = input()
+    if y == 1: return defaultPuzzle
+    elif y == 2: return makePuzzle()
+    else:
+        print "incorrect input"
+        return getPuzzle()
+
 
 def getAlg():
     print "     Enter your choice of algorithm"
@@ -36,9 +44,7 @@ def getAlg():
 
 def main():
     print "Welcome to Tyson Loveless' 8-puzzle solver."
-    print "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle."
-    puzzle = input()
-    thePuzzle = getPuzzle(puzzle)
+    thePuzzle = getPuzzle()
     for row in thePuzzle:
         print row
     print getAlg()
